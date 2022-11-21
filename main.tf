f96992449619b89ad1008e11b563b84f16eb5bc3
getazcloud <47303705+getazcloud@users.noreply.github.com>
2022-11-21 13:59:27 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,75 @@
+provider "azurerm" {
+  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used change
+  #version = "=2.4.0" without version
+
+  subscription_id = 00000000-2aa1-42f8-8726-6f8e1b32ab57
+  client_id       = 718f1aa6-1111-4480-ac5b-fbe59c70a130
+  client_secret   = 8c13bfe0-358f-0000-b2fe-ecb9d23bf644
+  tenant_id       = 8c13bfe0-0000-479e-b2fe-ecb9d23bf644
+
+  features {}
+}
+
+resource "azurerm_resource_group" "rgvnet" {
+  name     = "tfcloud"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = var.public
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
